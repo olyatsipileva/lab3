ef863a33c64490a0a91b52197386823816c34eed
Оля Ципилева <olya.tsipileva@yandex.ru>
2022-01-18 17:13:32 +0800

    1 часть сделана

---
+++ lab3/Temperature.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public enum MeasureType { F, C, K, Ra };
+
+    public class Temperature
+    {
+        private float value;
+        private MeasureType type;
+
+        public Temperature(float value, MeasureType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+
+        public string Verbose()
+        {
+            string typeVerbose = "";
+            switch (this.type)
+            {
+                case MeasureType.F:
+                    typeVerbose = "F";
+                    break;
+                case MeasureType.C:
+                    typeVerbose = "C";
+                    break;
+                case MeasureType.K:
+                    typeVerbose = "K";
+                    break;
+                case MeasureType.Ra:
+                    typeVerbose = "Ra";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+
+        public static Temperature operator +(Temperature instance, float number)
+        {
+            // расчитываем новую значение
+            var newValue = instance.value + number;
+            // создаем новый экземпляр класса, с новый значением и типом как у меры, к которой число добавляем
+            var length = new Temperature(newValue, instance.type);
+            // возвращаем результат
+            return length;
+        }
+
+        public static bool operator %(Temperature instance, float number)
+        {
+            // сравнение чисел
+            if (instance.value == number)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator %(float number, Temperature instance)
+        {
+            // сравнение чисел
+            if (instance.value == number)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // чтобы можно было добавлять число также слева
+        public static Temperature operator +(float number, Temperature instance)
+        {
+            // вызываем с правильным порядком аргументов, то есть сначала длина потом число
+            // для такого порядка мы определили оператор выше
+            return instance + number;
+        }
+
+        // умножение
+        public static Temperature operator *(Temperature instance, float number)
+        {
+            // мне лень по три строчки писать, поэтому я сокращаю код до одной строки
+            return new Temperature(instance.value * number, instance.type); ;
+        }
+
+        public static Temperature operator *(float number, Temperature instance)
+        {
+            return instance * number;
+        }
+
+        // вычитание
+        public static Temperature operator -(Temperature instance, float number)
+        {
+            return new Temperature(instance.value - number, instance.type); ;
+        }
+
+        public static Temperature operator -(float number, Temperature instance)
+        {
+            return instance - number;
+        }
+
+        // деление
+        public static Temperature operator /(Temperature instance, float number)
+        {
+            return new Temperature(instance.value / number, instance.type); ;
+        }
+
+        public static Temperature operator /(float number, Temperature instance)
+        {
+            return instance / number;
+        }
+
+        public static string Output(Temperature instance)
+        {
+            // перевод температуры
+            return instance.value.ToString() + ' ' + instance.type.ToString();
+        }
+
+        public Temperature To(MeasureType newType)
+        {
+            // по умолчанию новое значение совпадает со старым
+            var newValue = this.value;
+            // переводим в Цельсий
+            switch (this.type)
+            {
+                case MeasureType.C:
+                    newValue = this.value;
+                    break;
+                case MeasureType.F:
+                    newValue = (this.value - 32) / (9 / 5);
+                    break;
+                case MeasureType.K:
+                    newValue = this.value - 273;
+                    break;
+                case MeasureType.Ra:
+                    newValue = (this.value - 491.67f) / (9 / 5);
+                    break;
+            }
+            // если текущий тип -- это метр
+            if (this.type == MeasureType.C)
+            {
+                // а теперь рассматриваем все другие ситуации
+                switch (newType)
+                {
+                    // если конвертим в метр, то значение не меняем
+                    case MeasureType.C:
+                        break;
+                    // если в км.
+                    case MeasureType.F:
+                        newValue = newValue * 9 / 5 + 32;
+                        break;
+                    // если в  а.е.
+                    case MeasureType.K:
+                        newValue = newValue + 273;
+                        break;
+                    // если в парсек
+                    case MeasureType.Ra:
+                        newValue = newValue * 9 / 5 + 491.67f;
+                        break;
+                }
+            }
+            this.type = newType;
+            return new Temperature(newValue, newType);
+        }
+    }
+}
---
+++ lab3Tests/TemperatureTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Tests
+{
+    [TestClass()]
+    public class TemperatureTests
+    {
+        [TestMethod()]
+        public void PlusNumberTest()
+        {
+            var temperature = new Temperature(3, MeasureType.F);
+            temperature = temperature + 31f;
+            Assert.AreEqual("34 F", temperature.Verbose());
+        }
+
+        [TestMethod()]
+        public void SubNumberTest()
+        {
+            var temperature = new Temperature(3, MeasureType.F);
+            temperature = temperature - 1.75f;
+            Assert.AreEqual("1,25 F", temperature.Verbose());
+        }
+
+        [TestMethod()]
+        public void MulByNumberTest()
+        {
+            var temperature = new Temperature(3, MeasureType.C);
+            temperature = temperature * 3;
+            Assert.AreEqual("9 C", temperature.Verbose());
+        }
+
+        [TestMethod()]
+        public void DivByNumberTest()
+        {
+            var temperature = new Temperature(3, MeasureType.F);
+            temperature = temperature / 3;
+            Assert.AreEqual("1 F", temperature.Verbose());
+        }
+
+        [TestMethod()]
+        public void CompareByNumberTest()
+        {
+            var temperature = new Temperature(3, MeasureType.K);
+            bool compare = temperature % 3;
+            Assert.AreEqual("True K", compare.ToString() + " K");
+        }
+
+        [TestMethod()]
+        public void OutputTemperatureTest()
+        {
+            var temperature = new Temperature(3, MeasureType.Ra);
+            Assert.AreEqual(Temperature.Output(temperature), "3 Ra");
+        }
+
+        [TestMethod()]
+        public void CelsiusToAnyTestAnyToCelsius()
+        {
+            Temperature temperature;
+
+            temperature = new Temperature(9/5 + 32, MeasureType.F);
+            Assert.AreEqual("1 C", temperature.To(MeasureType.C).Verbose());
+
+            temperature = new Temperature(274, MeasureType.K);
+            Assert.AreEqual("1 C", temperature.To(MeasureType.C).Verbose());
+
+            temperature = new Temperature(9/5 + 491.67f, MeasureType.Ra);
+            Assert.AreEqual("1 C", temperature.To(MeasureType.C).Verbose());
+        }
+
+        [TestMethod()]
+        public void CelsiusToAnyTest()
+        {
+            Temperature temperature;
+
+            temperature = new Temperature(32, MeasureType.C);
+            Assert.AreEqual("89,6 F", temperature.To(MeasureType.F).Verbose());
+
+            temperature = new Temperature(1, MeasureType.C);
+            Assert.AreEqual("274 K", temperature.To(MeasureType.K).Verbose());
+
+            temperature = new Temperature(491.67f, MeasureType.C);
+            Assert.AreEqual("1376,676 Ra", temperature.To(MeasureType.Ra).Verbose());
+        }
+    }
+}
